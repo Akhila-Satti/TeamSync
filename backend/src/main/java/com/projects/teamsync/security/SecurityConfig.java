4e7a9a8641f779c33fb2f71abf6130cf668e21e3
Akhila <akhilastt@gmail.com>
2026-09-05 00:00:55 +0530

    cross origin configure

--- backend/src/main/java/com/projects/teamsync/security/SecurityConfig.java
+++ backend/src/main/java/com/projects/teamsync/security/SecurityConfig.java
@@ -10,6 +10,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 public class SecurityConfig {
 
@@ -23,13 +29,49 @@ public class SecurityConfig {
         public PasswordEncoder passwordEncoder() {
                 return new BCryptPasswordEncoder();
         }
+        @Bean
+public CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration configuration =
+            new CorsConfiguration();
+
+    configuration.setAllowedOrigins(
+            List.of("http://localhost:5173")
+    );
+
+    configuration.setAllowedMethods(
+            List.of(
+                    "GET",
+                    "POST",
+                    "PUT",
+                    "PATCH",
+                    "DELETE",
+                    "OPTIONS"
+            )
+    );
+
+    configuration.setAllowedHeaders(
+            List.of("*")
+    );
+
+    UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration(
+            "/**",
+            configuration
+    );
+
+    return source;
+}
 
         @Bean
         public SecurityFilterChain securityFilterChain(
                         HttpSecurity http) throws Exception {
 
-                http
-                                .csrf(AbstractHttpConfigurer::disable)
+              http
+        .cors(cors -> {})
+        .csrf(AbstractHttpConfigurer::disable)
 
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers(
